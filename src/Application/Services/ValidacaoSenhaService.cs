5ac6d68e4319c94593963ab80699fa55d9c25c9a
Mateus Back <m.back@nextage.com.br>
2025-04-15 20:07:56 -0300

    first commit

---
+++ src/Application/Services/ValidacaoSenhaService.cs
@@ -0,0 +1,29 @@
+﻿namespace Application.Services;
+
+public class ValidacaoSenhaService
+{
+    public IReadOnlyList<string> Validar(string senha)
+    {
+        var validacoes = new List<string>();
+        
+        if (senha.Length < 8)
+            validacoes.Add("A senha deve ter pelo menos 8 caracteres.");
+        
+        if (!senha.Any(char.IsUpper))
+            validacoes.Add("A senha deve conter pelo menos uma letra maiúscula.");
+        
+        if (!senha.Any(char.IsLower))
+            validacoes.Add("A senha deve conter pelo menos uma letra minúscula.");
+        
+        if (!senha.Any(char.IsDigit))
+            validacoes.Add("A senha deve conter pelo menos um número.");
+        
+        if (!senha.Any(char.IsSymbol) && !senha.Any(char.IsPunctuation))
+            validacoes.Add("A senha deve conter pelo menos um caractere especial.");
+        
+        if (senha.Contains(' '))
+            validacoes.Add("A senha não deve conter espaços.");
+        
+        return validacoes;
+    }
+}
---
+++ tests/Application.Tests/Services/ValidacaoSenhaService.cs
@@ -0,0 +1,87 @@
+﻿using Application.Services;
+
+namespace Application.Tests.Services;
+
+public class ValidacaoSenhaServiceTests
+{
+    private readonly ValidacaoSenhaService _validacaoSenhaService = new();
+
+    [Fact(DisplayName = "Deve retornar senha invalida caso tenha menos de 8 caracteres")]
+    public void DeveRetornarErroCasoSenhaPequena()
+    {
+        // Arrange
+        var senha = "1234567";
+        
+        // Act
+        var resultado = _validacaoSenhaService.Validar(senha);
+        
+        // Assert
+        Assert.Contains("A senha deve ter pelo menos 8 caracteres.", resultado);
+    }
+
+    [Fact(DisplayName = "Deve retornar senha invalida caso não tenha letras maiusculas")]
+    public void DeveRetornarFalseCasoNaoTenhaLetraMaiuscula()
+    {
+        // Arrange
+        var senha = "senha123";
+        
+        // Act
+        var resultado = _validacaoSenhaService.Validar(senha);
+        
+        // Assert
+        Assert.Contains("A senha deve conter pelo menos uma letra maiúscula.", resultado);
+    }
+
+    [Fact(DisplayName = "Deve retornar senha invalida caso não tenha letras minusculas")]
+    public void DeveRetornarFalseCasoNaoTenhaLetraMinuscula()
+    {
+        // Arrange
+        var senha = "SENHA123";
+        
+        // Act
+        var resultado = _validacaoSenhaService.Validar(senha);
+        
+        // Assert
+        Assert.Contains("A senha deve conter pelo menos uma letra minúscula.", resultado);
+    }
+
+    [Fact(DisplayName = "Deve retornar senha invalida caso não tenha numeros")]
+    public void DeveRetornarFalseCasoNaoTenhaNumeros()
+    {
+        // Arrange
+        var senha = "senhaSemNumero";
+        
+        // Act
+        var resultado = _validacaoSenhaService.Validar(senha);
+        
+        // Assert
+        Assert.Contains("A senha deve conter pelo menos um número.", resultado);
+    }
+
+    [Fact(DisplayName = "Deve retornar senha invalida caso não tenha caracteres especiais")]
+    public void DeveRetornarFalseCasoNaoTenhaCaracterEspecial()
+    {
+        // Arrange
+        var senha = "senhaInvalidaDeNovo123";
+        
+        // Act
+        var resultado = _validacaoSenhaService.Validar(senha);
+        
+        // Assert
+        Assert.Contains("A senha deve conter pelo menos um caractere especial.", resultado);
+    }
+
+    [Fact(DisplayName = "Deve retornar senha invalida caso tenha espaços em branco")]
+    public void DeveRetornarFalseCasoTenhaEspacosEmBranco()
+    {
+        // Arrange
+        var senha = "senha com espaco";
+        
+        // Act
+        var resultado = _validacaoSenhaService.Validar(senha);
+        
+        // Assert
+        Assert.Contains("A senha deve conter pelo menos um caractere especial.", resultado);
+
+    }
+}
